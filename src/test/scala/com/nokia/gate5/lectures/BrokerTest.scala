497a055b4b1252f5e266da957280c0b9e4b0a2cd
Lukasz Balamut <lukasz.balamut@nokia.com>
2012-01-09 01:54:34 +0100

    testing using value pooling

--- src/test/scala/com/nokia/gate5/lectures/BrokerTest.scala
+++ src/test/scala/com/nokia/gate5/lectures/BrokerTest.scala
@@ -3,7 +3,6 @@ package com.nokia.gate5.lectures
 import org.junit.Test
 import org.junit.Assert._
 
-
 class BrokerTest {
 
   @Test
@@ -13,10 +12,26 @@ class BrokerTest {
 
     b ! "new message"
 
-    Thread.sleep(100)
-
-    assertEquals("new message", b.value)
+    retryOnError(10) {
+      assertEquals("new message", b.value)
+    }
 
     b ! None //exits broker
+  }
+
+  def retryOnError(count: Int)(body: => Unit) = {
+    var retriesLeft = count
+    while (retriesLeft > 0) {
+      try {
+        retriesLeft = retriesLeft - 1;
+        body
+        retriesLeft = 0
+      } catch {
+        case ae: AssertionError => {
+          Thread.sleep(10)
+          if (retriesLeft <= 0) throw ae
+        }
+      }
+    }
   }
 }
